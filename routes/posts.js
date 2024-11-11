b136511952abcaabda88097352ea51c2035a4497
Dev Govindji <dev.govindji.software@gmail.com>
2024-11-10 22:54:41 -0600

    stubs json placeholder for posts call

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+const getPost = async () => {
+  //   console.log(await fetch(`https://jsonplaceholder.typicode.com/posts/`));
+  let posts = await fetch(`https://jsonplaceholder.typicode.com/posts/`)
+    .then((response) => response.json())
+    .then((posts) => console.log("posts: ", posts));
+  return posts;
+  //   await fetch(`https://jsonplaceholder.typicode.com/posts/`)
+  //     .then((response) => (posts = response.json()))
+  //     .then((posts) => console.log("posts: ", posts));
+  //   return posts;
+};
+
 // Define a route
 router.get("/", (req, res) => {
-  res.send("<h3>This is a Posts route!</h3>");
+  //   res.json({ data: getPost() });
+
+  //   res.json({ data: getPost() });
+  //   res.json({ username: "Flavio" });
+  //   res.end(JSON.stringify(getPost()));
+  //   res.json(getPost());
+  res.send(`<h3>This is a Posts route! ${getPost()}</h3>`);
 });
 
 router.get("/101", (req, res) => {
